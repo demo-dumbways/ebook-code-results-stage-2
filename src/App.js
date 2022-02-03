6d9bcb6af8e3a6f3f9c190fa01ae1dbe38aa800f
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-02-03 17:14:57 +0700

    9 frontend reactjs fundamental

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-//Create Component PrivatePage
 function PrivatePage(props) {
   return (
     <div>
       <h1>Welcome</h1>
-      {/* Parsing using Props */}
       <button onClick={props.logout}>Logout</button>
     </div>
   );
 }
 
-//Create Component GuestPage
 function GuestPage(props) {
   return (
     <div>
       <h1>Please Sign</h1>
-      {/* Parsing using Props */}
       <button onClick={props.login}>Login</button>
     </div>
   );
 }
 
-function ConditionRenderin() {
-  //init State
+function App() {
   const [isLoggedin, setIsLoggedin] = useState(false);
   return (
     <div>
-      {/* conditional logic rendering */}
       {isLoggedin ? (
         <PrivatePage logout={() => setIsLoggedin(!isLoggedin)} />
       ) : (
@@ -37,4 +31,4 @@ function ConditionRenderin() {
   );
 }
 
-export default ConditionRenderin;
+export default App;
