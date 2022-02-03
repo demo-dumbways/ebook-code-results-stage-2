4fb402619e4aeaaa938183e335c6ed53e8cc655f
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-02-03 16:25:08 +0700

    7 frontend reactjs fundamental

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div>
       <p>On the image element using the default props, namely src</p>
-      <img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1542354895/ulaqus4ev5ihhqkpbhuz.jpg" />
+      <img
+        alt="image"
+        src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1542354895/ulaqus4ev5ihhqkpbhuz.jpg"
+      />
 
       <List data="BMW" />
       <List data="Mercedes-Benz" />
