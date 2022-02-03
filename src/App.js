7a4692b3ae1eaf044d44490e53c94af5d4537e24
samsul-rijal <samsulr99@gmail.com>
2022-02-03 22:37:35 +0700

    1 frontend reactjs advance

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { useState } from 'react';
-
-function PrivatePage(props) {
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <button onClick={props.logout}>Logout</button>
-    </div>
-  );
-}
-
-function GuestPage(props) {
-  return (
-    <div>
-      <h1>Please Sign</h1>
-      <button onClick={props.login}>Login</button>
-    </div>
-  );
-}
+import Form from './components/Form';
 
 function App() {
-  const [isLoggedin, setIsLoggedin] = useState(false);
   return (
     <div>
-      {isLoggedin ? (
-        <PrivatePage logout={() => setIsLoggedin(!isLoggedin)} />
-      ) : (
-        <GuestPage login={() => setIsLoggedin(!isLoggedin)} />
-      )}
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+const styles = {
+    form: {
+        margin: '16px 20% 0',
+    },
+    formGroup: {
+        marginBottom: '16px',
+    },
+    formLabel: {
+        marginBottom: '8px',
+        display: 'inline-block',
+    },
+    formInput: {
+        display: 'block',
+        width: '100%',
+        padding: '.375rem .75rem',
+        fontSize: '1rem',
+        lineHeight: 1.5,
+        color: '#212529',
+        backgroundColor: '#fff',
+        border: '1px solid #ced4da',
+        borderRadius: '.25rem',
+    },
+};
+  
+function Form() {
+    return (
+        <form style={styles.form}>
+        <div style={styles.formGroup}>
+            <label htmlFor="username" style={styles.formLabel}>
+            Username
+            </label>
+            <input
+            id="username"
+            placeholder="Input username"
+            style={styles.formInput}
+            />
+        </div>
+        </form>
+    );
+}
+
+export default Form;
